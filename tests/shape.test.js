bccc0a81ae3af143f7a570ecacd14ecf85590380
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-07-29 23:04:30 +1000

    Updated tests

--- tests/shape.test.js
+++ tests/shape.test.js
@@ -3,23 +3,32 @@ const Shape = require('../lib/shape.js');
 describe('Circle', () => {
   test('render message', () => {
     const shape = new Shape.Cirlce();
+    shape.text = 'abc';
+    shape.text_colour = 'white';
     shape.shape_colour = 'red';
-    expect(shape.render()).toEqual(expect.arrayContaining(`<circle cx="50" cy="70" r="40" fill="red"></circle>`));
+    expect(shape.render()).toEqual(`<circle cx="50" cy="70" r="40" fill="red"></circle>
+    <text x="50" y="75" font-size="25" text-anchor="middle" fill="white">abc</text>`);
   });
 });
 
 describe('Triangle', () => {
   test('render message', () => {
     const shape = new Shape.Triangle();
-    shape.shape_colour = 'blue';
-    expect(shape.render()).toEqual(expect.arrayContaining(`<polygon points="50,15, 100,100 0,100" fill="blue"></polygon>)`));
+    shape.text = 'HIJ';
+    shape.text_colour = 'yellow';
+    shape.shape_colour = 'dark blue';
+    expect(shape.render()).toEqual(`<polygon points="50,15, 100,100 0,100" fill="dark blue"></polygon>
+    <text x="50" y="75" font-size="25" text-anchor="middle" fill="yellow">HIJ</text>`);
   });
 });
 
 describe('Square', () => {
   test('render message', () => {
     const shape = new Shape.Square();
+    shape.text = 'HA';
+    shape.text_colour = 'red';
     shape.shape_colour = 'black';
-    expect(shape.render()).toEqual(expect.arrayContaining(`<circle cx="50" cy="70" r="40" fill="black"></circle>`));
+    expect(shape.render()).toEqual(`<rect x="10" y="28" width="80" height="80" fill="black"></rect>
+    <text x="50" y="75" font-size="25" text-anchor="middle" fill="red">HA</text>`);
   });
 });
--- tests/svg.test.js
+++ tests/svg.test.js
@@ -2,8 +2,12 @@ const {createShape} = require('../lib/svg.js');
 
 describe('svg_test', () => {
   test('render message', () => {
-    expect(createShape()).toEqual(expect.arrayContaining(`<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-    <svg viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">`));
+    createShape('ABC', "white", "Circle", "red");
+    expect(createShape()).toEqual(`<?xml version="1.0" encoding="UTF-8" standalone="no"?> 
+    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> 
+    <svg viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
+     <circle cx="50" cy="70" r="40" fill="red"></circle>
+     <text x="50" y="75" font-size="25" text-anchor="middle" fill="white">ABC</text>
+    </svg>`);
   });
 });
