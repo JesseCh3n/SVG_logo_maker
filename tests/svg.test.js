b2361f114354613f2d115c009bb6ce005f1a0498
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-07-30 17:16:08 +1000

    updated tests

--- tests/svg.test.js
+++ tests/svg.test.js
@@ -1,13 +1,16 @@
 const {createShape} = require('../lib/svg.js');
+const Shape = require('../lib/shape.js');
 
 describe('svg_test', () => {
   test('render message', () => {
-    createShape('ABC', "white", "Circle", "red");
-    expect(createShape()).toEqual(`<?xml version="1.0" encoding="UTF-8" standalone="no"?> 
-    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> 
+    const newSVG = createShape('ABC', "white", "Circle", "red");
+    const newCircle = new Shape.Cirlce('ABC', 'white', 'Circle', 'red').render();
+    const expectedSVG = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
     <svg viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-     <circle cx="50" cy="70" r="40" fill="red"></circle>
-     <text x="50" y="75" font-size="25" text-anchor="middle" fill="white">ABC</text>
-    </svg>`);
+      ${newCircle}
+    </svg>
+    `;
+    expect(newSVG).toEqual(expectedSVG);
   });
 });
