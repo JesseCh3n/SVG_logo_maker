3b55d7ec49b54db93b03e977577393b74859653a
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-08-01 22:09:57 +1000

    Completion

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # SVG_logo_maker
+ ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+
+This application generates a SVG file from command line inputs.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [How to Contribute](#how%20to%20contribute)
+- [Tests](#tests)
+
+## Installation
+
+N/A
+
+## Usage
+
+Please run the NodeJS application in a terminal. Follow the command line prompts.
+![alt screenshot](./image.gif)
+
+## Credits
+
+Credit to Coding Bootcamp.
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+
+## How to Contribute
+
+N/A
+
+  ## Tests
+
+  N/A
--- tests/svg.test.js
+++ tests/svg.test.js
@@ -5,12 +5,7 @@ describe('svg_test', () => {
   test('render message', () => {
     const newSVG = createShape('ABC', "white", "Circle", "red");
     const newCircle = new Shape.Cirlce('ABC', 'white', 'Circle', 'red').render();
-    const expectedSVG = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-    <svg viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">
-      ${newCircle}
-    </svg>
-    `;
+    const expectedSVG = `<?xml version="1.0" encoding="UTF-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"><svg viewBox="0 0 150 150" xmlns="http://www.w3.org/2000/svg">${newCircle}</svg>`;
     expect(newSVG).toEqual(expectedSVG);
   });
 });
